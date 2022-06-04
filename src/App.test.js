6c37d1909b0273be20ff0902e00f9e8e5968f3fb
zeshan7061 <zeshan7061@gmail.com>
2022-06-04 11:21:34 +0500

    initial app setup with 2 tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+	render(<App />)
+	// const linkElement = screen.getByText(/learn react/i)
+	const linkElement = screen.getByTestId('test-link')
+	expect(linkElement).toBeInTheDocument()
+})
+
+test('link contains react site url', () => {
+	render(<App />)
+	// const linkElement = screen.getByText(/learn react/i)
+	const linkElement = screen.getByTestId('test-link')
+	expect(linkElement.href).toContain('https://reactjs.org')
+})
